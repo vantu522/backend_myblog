a4bb774fb562d666c9005fd51e5a54e5f45bc89a
tudgvn23 <tuduongvan2305@gmail.com>
2025-02-19 09:51:57 +0700

    update build config

---
+++ label.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    presets: [
+      ['@babel/preset-env', {
+        targets: {
+          node: 'current'
+        }
+      }]
+    ],
+    plugins: [
+      '@babel/plugin-transform-runtime',
+      ['module-resolver', {
+        root: ['./src'],
+        alias: {
+          '@': './src'
+        }
+      }]
+    ]
+  }
