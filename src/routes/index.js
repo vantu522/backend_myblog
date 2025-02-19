451f648ee011061f9b69afdefc3cb11a28d673d4
tudgvn23 <tuduongvan2305@gmail.com>
2025-02-19 10:40:33 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import postRouter from './post.route'
 function route(app) {
     app.use('/auth', authRouter)
     app.use('/users', userRouter)
-    app.use('/post', postRouter)
+    app.use('/p', postRouter)
 }
 
 export default route 
